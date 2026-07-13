76844afa0946130acc5061f226fa8d11bcc2cb84
sudhakarpogula <sudhakarreddyp1999@gmail.com>
2026-07-13 17:43:18 +0530

    Create hello.java

---
+++ multi-stage-multi-agent/hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Java!");
+        System.out.println("This program is executed by Jenkins.");
+    }
+}
